4dbf08b99df015e87a4480f96308c73e7d6a7325
Rachel Gardner <rachelgardner9@gmail.com>
2020-12-02 19:13:52 -0800

    Add automated invite functionality

--- members.py
+++ members.py
@@ -1,7 +1,8 @@
 from discord.ext import commands
+import discord
 import pandas as pd 
 import os.path
-
+import asyncio
 
 
 if not os.path.exists("./memberInfo.csv"):
@@ -11,11 +12,59 @@ if not os.path.exists("./memberInfo.csv"):
     df.to_csv("./memberInfo.csv", index=False)
 
 
+SERVER_ID = 744657737027158166
+    
 class Members(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.df = pd.read_csv("./memberInfo.csv", index_col='Nickname')
+        self.invite_delay_hours = 0.001
+        self.live_invites = set()
 
+    @commands.command(name='cancel_invite', help='Cancel an invite, preventing a link from being generated.')        
+    async def cancel_invite(self, ctx, *args):
+        user_name = ' '.join(args)
+        try:
+            self.live_invites.remove(user_name)
+        except KeyError:
+            await ctx.send(f"'{user_name}' was not being invited. Maybe the spelling/capitalization is off?")
+        else:    
+            await ctx.send(f"{user_name} is no longer being invited.")
+        
+    @commands.command(name='invite', help='Propose inviting a certain person')        
+    async def invite(self, ctx, *args):
+        """
+        Start the invite process for a certain person. This starts a timer of invite_delay_hours, then after the
+        timer is up, generates a custom link to send to the person who called this command.
+        usage: .invite Anna Zeng
+        """
+        user_name = ' '.join(args)
+
+        # Trigger the comment period for a given proposed invite.
+        await ctx.send(f"The invite process for {user_name} has begun! You now have {self.invite_delay_hours} hours to use .feedback to anonymously veto the person for any reason before they are automatically invited as a permanent resident.")
+        self.live_invites.add(user_name)
+
+        # Wait the appropriate number of hours.
+        await asyncio.sleep(60*60*self.invite_delay_hours)
+
+        # If the invite has been cancelled, don't do anything.
+        if user_name not in self.live_invites: return
+
+        # Since the invite wasn't cancelled, go ahead and generate an invite link.
+        channel = discord.utils.get(ctx.guild.channels, name='welcome')
+        custom_link = await channel.create_invite(unique=True)
+        await ctx.send(f"I'm now inviting {user_name}! <@{ctx.author.id}>, I'll be sending you a custom link to forward to {user_name}")
+        await ctx.author.send(f"Here is the link for inviting {user_name}!: {custom_link}")
+        self.live_invites.remove(user_name)
+
+        # Cache invite information for role assignment logic when the new member joins.
+        self.bot.resident_invites.add((ctx.author, custom_link.code))
+        self.bot.invite_cache[ctx.guild.id] = {}
+        invites = await ctx.guild.invites()
+        for invite in invites:
+            self.bot.invite_cache[ctx.guild.id][invite.code] = invite
+
+        
     ## Note: if we have duplicate nicknames this will be bad lol
     @commands.command(name='set_email', help='Lets Tomi know what your email is! Use: set_email bob@gmail.com')
     async def set_email(self, ctx, email):
--- spreadsheet.py
+++ spreadsheet.py
@@ -16,6 +16,36 @@ class Spreadsheet():
         self.idx_offset = 2 # pandas_idx + idx_offset = spreadsheet_idx
         self.service = discovery.build('sheets', 'v4', credentials=credentials)
 
+    def add_invitee(self, inviter, invitee):
+        inviter = f"{inviter.name} ({inviter.id})"
+        invitee = f"{invitee.name} ({invitee.id})"
+
+        range_name = 'Family Tree!A2:A'
+        response = self.service.spreadsheets().values().get(spreadsheetId=self.spreadsheet_id, range=range_name).execute()
+        tracked_users = response.get('values', [[]])
+        
+        user_tracked = False
+        
+        row_idx = 2
+        for user in tracked_users[0]:
+            row_idx += 1
+            if user == inviter:
+                user_tracked = True
+                break
+
+        if not user_tracked:
+            range_name = f'Family Tree!A{row_idx}:B{row_idx}'
+            self.service.spreadsheets().values().update(spreadsheetId=self.spreadsheet_id,
+                                                        range=range_name, valueInputOption='RAW',
+                                                        body=dict(values=[[inviter, invitee]])).execute()
+        else:
+            range_name = f'Family Tree!B{row_idx}:B{row_idx}'
+            response = self.service.spreadsheets().values().get(spreadsheetId=self.spreadsheet_id, range=range_name).execute()
+            previous_invitees = response['values'][0][0]
+            all_invitees = previous_invitees + ',' + invitee
+            self.service.spreadsheets().values().update(spreadsheetId=self.spreadsheet_id, valueInputOption='RAW',
+                                                        range=range_name, body=dict(values=[[all_invitees]])).execute()
+            
     def spreadsheet_to_pandas(self, name, start_col, end_col):
         """
         Reads the spreadsheet with the given name from start_col to end_col into a pandas DataFrame and returns it.
@@ -29,6 +59,16 @@ class Spreadsheet():
         data = response['values']
         return pd.DataFrame(data[1:], columns=data[0])
 
+    def add_question(self, author, question, spiciness):
+        range_name = f'Questions!A:A'
+        response = self.service.spreadsheets().values().get(spreadsheetId=self.spreadsheet_id, range=range_name).execute()
+        row_idx = len(response['values']) + 1
+        range_name = f'Questions!A{row_idx}:D{row_idx}'
+        self.service.spreadsheets().values().update(spreadsheetId=self.spreadsheet_id, valueInputOption='RAW',
+                                                   range=range_name,
+                                                    body=dict(values=[[question, spiciness, '', author.name]])).execute()
+        return True
+            
     def get_question(self, spiciness):
         """
         Returns an unasked question at the given spiciness level or lower, or an error message is returned.
