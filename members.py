46cf69a704c41cbfd61306955aed7ee2b9186f90
Zheng <yzh@stanford.edu>
2020-12-02 19:20:32 -0800

    added voice channel function

--- members.py
+++ members.py
@@ -1,14 +1,57 @@
 from discord.ext import commands
 import pandas as pd 
 import os.path
+from random import randint
 
+CSV_LOC = "./memberInfo.csv"
 
-
-if not os.path.exists("./memberInfo.csv"):
-    d = {'Nickname':[], 'Email': [], 'Venmo': []};
+if not os.path.exists(CSV_LOC):
+    d = {'Nickname':[], 'Email': [], 'Venmo': [], 'Playlist' : [], 'Autoplay' : []}
     df = pd.DataFrame(data=d)
     # In .gitignore
-    df.to_csv("./memberInfo.csv", index=False)
+    df.to_csv(CSV_LOC, index=False)
+
+df = pd.read_csv(CSV_LOC, index_col='Nickname')
+
+ # Called when user joins Library and Groovy is not already a part of it
+async def autoplay_playlist_helper(channel, voice_channel, author, curr_voice_client):
+    print("autoplay trying")
+    user = author.name
+    if user in df.index:
+        playlist = df.at[user, 'Playlist']
+        autoplay = df.at[user, 'Autoplay']
+        if playlist is not None and str(autoplay) == "True":
+            print("Playing ", playlist)
+            token = randint(1,3)
+            if token == 1:
+                await channel.send(f"{user} just connected to the library. Let's jam!")
+            elif token == 2:
+                await channel.send(f"Hooray to {user} for being productive today! Let's get started!")
+            else:
+                await channel.send(f"Study time for {user}! How about some tunes? :D")
+
+            if curr_voice_client is not None:
+                await curr_voice_client.disconnect()
+
+            await play_playlist_helper(channel, voice_channel, user, df)
+ 
+async def play_playlist_helper(channel, voice_channel, user, df):
+    print(channel)
+    print(voice_channel)
+    print(user)
+
+    if user not in df.index or df.at[user, 'Playlist'] is None:
+        print("No user registered playlist")
+        await channel.send(f"{user} does not have a registered playlist!")
+        return
+    # If you are in a voice channel
+    playlist = df.at[user, 'Playlist']
+    print("Connecting to voice channel!")
+    await voice_channel.connect()
+
+    await channel.send("Note: Groovy ignores other bots, so please copy/paste the command below:")
+    await channel.send(f"-play {playlist}")
+
 
 
 class Members(commands.Cog):
@@ -21,7 +64,6 @@ class Members(commands.Cog):
     async def set_email(self, ctx, email):
         await self.set_val(ctx, "Email", email)
 
-
     @commands.command(name='get_email',
                  help='Gets email for a member. Use: get_email for yourself, get_email bob for bob')
     async def get_email(self, ctx, *args):
@@ -29,7 +71,6 @@ class Members(commands.Cog):
             await self.get_val(ctx, "Email", ctx.author.name)
         else:
             await self.get_val(ctx, "Email", args[0])
-        
 
     @commands.command(name='set_venmo', help='Lets Tomi know what your venmo is! Use: set_venmo bob3')
     async def set_venmo(self, ctx, venmo):
@@ -43,6 +84,46 @@ class Members(commands.Cog):
             await self.get_val(ctx, "Venmo", ctx.author.name)
         else:
             await self.get_val(ctx, "Venmo", args[0])
+
+    @commands.command(name='set_playlist', help='Lets Tomi know what playlist you want saved!')
+    async def set_playlist(self, ctx, playlist):
+        await self.set_val(ctx, "Playlist", playlist)
+        await ctx.send("Want Tomi to auto-play your playlist in Library? Do .autoplay_on!")
+
+
+    @commands.command(name='get_playlist',
+                 help='Gets playlists for a member. Use: get_playlist for yourself, get_playlist bob for bob')
+    async def get_playlist(self, ctx, *args):
+        if len(args) == 0:
+            await self.get_val(ctx, "Playlist", ctx.author.name)
+        else:
+            await self.get_val(ctx, "Playlist", args[0])
+
+    @commands.command(name='play_playlist',
+                 help='Gets playlists for a member. Use: get_playlist for your playlist, get_playlist bob for bob playlist')
+    async def play_playlist(self, ctx, *args):
+        user = ctx.author.name
+        if len(args) > 0:
+            user = args[1]
+        if ctx.author.voice and ctx.author.voice.channel:
+            voice_channel = ctx.author.voice.channel
+            curr_voice_client = ctx.message.guild.voice_client
+            if curr_voice_client is not None:
+                await curr_voice_client.disconnect()
+            await play_playlist_helper(ctx, voice_channel, user, self.df)
+        else:
+            await ctx.send("You are not in a voice channel.")
+        
+
+    @commands.command(name='autoplay_on',
+                 help='Sets autoplay-upon-entry to library to on [note -- does not work right now]')
+    async def autoplay_on(self, ctx):
+         await self.set_val(ctx, "Autoplay", True)
+    
+    @commands.command(name='autoplay_off',
+                 help='Sets autoplay-upon-entry to library to off [note -- does not work right now]')
+    async def autoplay_off(self, ctx):
+         await self.set_val(ctx, "Autoplay", False)
     
 
     async def get_val(self, ctx, field, name):
@@ -54,9 +135,10 @@ class Members(commands.Cog):
         await ctx.send(f"{field} not found for: {name}")
 
     async def set_val(self, ctx, field, value):
-        self.df.at[ctx.author.name, field] = value
+        self.df.loc[ctx.author.name, field] = value
         await ctx.send(f"Updated {ctx.author.name} with {field}: {value} ")
         self.df.to_csv("./memberInfo.csv")
+
    
 def setup(bot):
     bot.add_cog(Members(bot))
